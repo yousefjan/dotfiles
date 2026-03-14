d0d7077215f914b9564ff6a5f302aff8a02942d9
y-jan137 <yousefjan24000@gmail.com>
2026-03-14 15:56:21 +0300

    Updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,32 @@
--- theme & transparency
+-- Plugins
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    keys = {
+      { '<leader>ff', '<cmd>Telescope find_files<cr>' },
+      { '<leader>fg', '<cmd>Telescope live_grep<cr>' },
+      { '<leader>fb', '<cmd>Telescope buffers<cr>' },
+    },
+  },
+})
+```
+
+-- Theme & transparency
 vim.cmd.colorscheme("unokai")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
